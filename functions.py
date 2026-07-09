7c3e648805df336657fbee35fecc3891e2f3f167
jennifereliasch <143544735+jennifereliasch@users.noreply.github.com>
2026-07-09 17:05:16 +0200

    HQ Axial_function

--- functions.py
+++ functions.py
@@ -169,3 +169,24 @@ def evaluate_pulsatile(path, rpm, t_start, t_end):
     fig.suptitle(f"Pulsatiler Betrieb - {rpm} rpm")
     plt.tight_layout()
     plt.show()
+
+    # HQ Diagramm der Axialpumpe
+    def plot_axial_curve(axial_data):
+
+    plt.figure(figsize=(10,6))
+
+    for rpm, values in axial_data.items():
+
+        Q = values["Q"]
+        H = values["H"]
+        Q, H = zip(*sorted(zip(Q, H)))
+
+        plt.plot(Q, H, "o-", label=f"{rpm} rpm")
+
+    plt.xlabel("Volumenstrom Q [L/min]")
+    plt.ylabel("Druckdifferenz H [mmHg]")
+    plt.title("H-Q-Diagramm Axialpumpe")
+    plt.grid(True)
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
